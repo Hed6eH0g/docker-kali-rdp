90a7831e1c66af88c6583337509eac410b364869
Hed6eH0g <102146327+Hed6eH0g@users.noreply.github.com>
2025-02-19 19:21:19 +0900

    Add files via upload
    initial commit

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+KBLAYOUT=$1
+ROOTPW=$2
+
+if [ ! -e '/check' ]; then
+    touch /check
+    setxkbmap ${KBLAYOUT}
+    usermod --password $(echo $ROOT_PASSWD | openssl passwd -1 -stdin) root
+fi
+
+if [ -f /var/run/xrdp/xrdp.pid ]; then
+    rm /var/run/xrdp/xrdp.pid 
+fi
+if [ -f /var/run/xrdp/xrdp-sesman.pid ]; then
+    rm /var/run/xrdp/xrdp-sesman.pid 
+fi
+/etc/init.d/xrdp start
